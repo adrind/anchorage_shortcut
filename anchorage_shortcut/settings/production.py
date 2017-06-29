b371fd9268a7d69edd856a4de178d29bc6d99af9
Adrienne <adrienne@codeforamerica.org>
2017-06-29 16:57:35 -0700

    updated settings for heroku static asset mgmt

--- anchorage_shortcut/settings/production.py
+++ anchorage_shortcut/settings/production.py
@@ -17,6 +17,15 @@ DEBUG = False
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 try:
     from .local import *
 except ImportError:
