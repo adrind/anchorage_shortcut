7776426654a0fda0597ce8dc04247f12e21e4052
Adrienne <adrienne@codeforamerica.org>
2017-08-08 11:03:46 -0700

    Revert "Removed uneeded POST for the choice forms"
    This reverts commit 10884897ec2808c7f6e4f97e34a4f3ce5a5bbd7f.

--- roadmap/models.py
+++ roadmap/models.py
@@ -211,11 +211,9 @@ class TaskList(Page):
     def serve(self, request, template=''):
         if template == '':
             template = self.template
-        #Kind of hacky but intercept request if it has 'submit-choice' in the slug
-        #Serve the rules for the selected choices
-        if len(request.GET) and "submit-choice" in request.path.split('/'):
+        if request.method == 'POST':
             #Get selected checkbox values from form in request - need to strip out csrf token
-            selected_choices = list(request.GET.values())
+            selected_choices = list(filter(None, (map(lambda x: x.split('=')[1].replace('+', ' ') if x.split('=')[0] != 'csrfmiddlewaretoken' else '', request.POST.urlencode().split('&')))))
 
             #Sort the choices so we have them in the same order as the admin defined rules
             selected_choices.sort()
@@ -255,8 +253,8 @@ class TaskList(Page):
                         break
                     if rule.value['name'] in selected_choices:
                         for i, page in enumerate(rule.value['pages']):
-                            if page.id not in pages:
-                                pages.append(page.id)
+                            if page not in pages:
+                                pages.append(page)
                 for i, page in enumerate(pages):
                     ids.append(str(page.id))
 
@@ -276,7 +274,7 @@ class TaskList(Page):
                 'stepIds' : ','.join(ids),
                 'default_pages': default_pages
             })
-        #Otherwise just render the track page with the appropriate template
+
         return render(request, template, {
             'page': self
         })
