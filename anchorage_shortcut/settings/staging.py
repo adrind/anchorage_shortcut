4eb1e1a2f904b7045b68985bf207b1c863ef6f3f
Adrienne <adrienne@codeforamerica.org>
2017-08-16 11:55:42 -0800

    added database url to staging env

--- anchorage_shortcut/settings/staging.py
+++ anchorage_shortcut/settings/staging.py
@@ -32,6 +32,8 @@ ALGOLIA['INDEX_PREFIX'] = 'prod'
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+env['DATABASE_URL'] = 'postgres://jxpyiubojggyqx:6bb7a6cfb97af5e9ba749c0a83f7dcb6cff147b3b0647b005cabb115bc82854b@ec2-54-83-26-65.compute-1.amazonaws.com:5432/d98a74fiqpdm8s'
+
 try:
     from .local import *
 except ImportError:
