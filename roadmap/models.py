4997c3bb6f2f6d50ccda7ce15a6149a732afd4ed
Adrienne <adrienne@codeforamerica.org>
2017-08-16 11:07:09 -0800

    removed icon from steppage

--- roadmap/models.py
+++ roadmap/models.py
@@ -342,8 +342,6 @@ class StepPage(Page):
     body = RichTextField(blank=True)
     next_step = models.URLField(blank=True)
 
-    icon = models.CharField(choices=ICON_CHOICES, blank=True, max_length=255, default='')
-
     address = models.CharField(max_length=250, blank=True, null=True)
     location = models.CharField(max_length=250, blank=True, null=True)
 
@@ -358,7 +356,6 @@ class StepPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('short_description', classname='full'),
         FieldPanel('body', classname='full'),
-        FieldPanel('icon'),
         InlinePanel('related_resources', label='Extra resources'),
         InlinePanel('faqs', label='Frequently asked questions'),
         SnippetChooserPanel('contact'),
